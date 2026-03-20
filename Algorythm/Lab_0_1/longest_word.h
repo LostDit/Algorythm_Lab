bc68f2e5c128bd2b53277da62f4ed1fe4f691d83
LostDit <LostDit>
2026-03-20 13:37:34 +0300

    Делаю более приятный нейминг через camelCase

--- Algorythm/Lab_0_1/longest_word.h
+++ Algorythm/Lab_0_1/longest_word.h
@@ -14,20 +14,21 @@ inline void run() {
     std::string line;
     std::getline(std::cin, line);
 
-    std::vector<std::string> longest_words;
-    size_t max_len = 0;
+    std::vector<std::string> longestWords;
+    size_t maxLen = 0;
     std::string word;
 
     for (char ch : line) {
+ 
         if (ch == ' ' || ch == '.' || ch == ',') {
             if (!word.empty()) {
-                if (word.length() > max_len) {
-                    max_len = word.length();
-                    longest_words.clear();
-                    longest_words.push_back(word);
+                if (word.length() > maxLen) {
+                    maxLen = word.length();
+                    longestWords.clear();
+                    longestWords.push_back(word);
                 }
-                else if (word.length() == max_len) {
-                    longest_words.push_back(word);
+                else if (word.length() == maxLen) {
+                    longestWords.push_back(word);
                 }
                 word.clear();
             }
@@ -38,21 +39,21 @@ inline void run() {
     }
 
     if (!word.empty()) {
-        if (word.length() > max_len) {
-            longest_words.clear();
-            longest_words.push_back(word);
+        if (word.length() > maxLen) {
+            longestWords.clear();
+            longestWords.push_back(word);
         }
-        else if (word.length() == max_len) {
-            longest_words.push_back(word);
+        else if (word.length() == maxLen) {
+            longestWords.push_back(word);
         }
     }
 
-    if (longest_words.empty()) {
+    if (longestWords.empty()) {
         std::cout << "Слов не найдено." << std::endl;
     }
     else {
         std::cout << "Самое длинное слово (все варианты):" << std::endl;
-        for (const auto& w : longest_words) {
+        for (const auto& w : longestWords) {
             std::cout << w << std::endl;
         }
     }
